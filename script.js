3fe3df568c7652a691549e872a499c5e5268837c
Kowabongaa <158025851+Kowabongaa@users.noreply.github.com>
2024-11-25 14:48:37 +0800

    d

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function scrollToProducts() {
 function addToCart(productName) {
     alert(productName + ' has been added to your cart!');
 }
+
+window.addEventListener('scroll', function() {
+    const heroSection = document.querySelector('.hero');
+    const productsSection = document.querySelector('#products');
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > heroSection.offsetTop && scrollPosition < productsSection.offsetTop) {
+        heroSection.style.opacity = '0.9';
+    } else {
+        heroSection.style.opacity = '1';
+    }
+});
