2b626d3de170c904e411ebfcdbfe137be9929f7d
Kowabongaa <158025851+Kowabongaa@users.noreply.github.com>
2024-11-25 14:56:27 +0800

    a

--- script.js
+++ script.js
@@ -8,10 +8,9 @@ function addToCart(productName) {
 
 window.addEventListener('scroll', function() {
     const heroSection = document.querySelector('.hero');
-    const productsSection = document.querySelector('#products');
     const scrollPosition = window.scrollY;
 
-    if (scrollPosition > heroSection.offsetTop && scrollPosition < productsSection.offsetTop) {
+    if (scrollPosition > heroSection.offsetTop && scrollPosition < heroSection.offsetTop + heroSection.clientHeight) {
         heroSection.style.opacity = '0.9';
     } else {
         heroSection.style.opacity = '1';
