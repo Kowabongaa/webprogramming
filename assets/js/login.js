39f26d947484a9d3738018fef702ac2197bac6d8
Kowabongaa <158025851+Kowabongaa@users.noreply.github.com>
2025-01-01 20:53:15 +0800

    a

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,30 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const loginForm = document.getElementById('login-form');
-    const loginError = document.getElementById('login-error');
-  
-    // Handle login form submission
-    loginForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-  
-      const username = document.getElementById('username').value;
-      const password = document.getElementById('password').value;
-  
-      // Retrieve users from localStorage
-      const users = JSON.parse(localStorage.getItem('users')) || [];
-  
-      // Validate credentials
-      const user = users.find((user) => user.username === username && user.password === password);
-      if (user) {
-        // Store login state
-        localStorage.setItem('userLoggedIn', 'true');
-        localStorage.setItem('username', username);
-  
-        // Redirect to main page
-        window.location.href = 'index.html'; // Change this to your main page URL
-      } else {
-        // Show error message
-        loginError.textContent = 'Invalid username or password. Please try again.';
-      }
-    });
-  });
-  
+/*===== LOGIN SHOW and HIDDEN =====*/
+const signUp = document.getElementById('sign-up'),
+    signIn = document.getElementById('sign-in'),
+    loginIn = document.getElementById('login-in'),
+    loginUp = document.getElementById('login-up')
+
+
+signUp.addEventListener('click', ()=>{
+    // Remove classes first if they exist
+    loginIn.classList.remove('block')
+    loginUp.classList.remove('none')
+
+    // Add classes
+    loginIn.classList.toggle('none')
+    loginUp.classList.toggle('block')
+})
+
+signIn.addEventListener('click', ()=>{
+    // Remove classes first if they exist
+    loginIn.classList.remove('none')
+    loginUp.classList.remove('block')
+
+    // Add classes
+    loginIn.classList.toggle('block')
+    loginUp.classList.toggle('none')
+})
