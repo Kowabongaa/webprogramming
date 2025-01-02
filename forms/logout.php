acd5114dabe3a7b44a65d128823eb1ab59036aa0
Kowabongaa <158025851+Kowabongaa@users.noreply.github.com>
2025-01-02 18:45:34 +0800

    a

--- forms/logout.php
+++ forms/logout.php
@@ -1,6 +1,12 @@
 <?php
+
 session_start();
-session_destroy();
-header('Location: login.html');
-exit;
-?>
+
+if(isset($_SESSION['user_id']))
+{
+	unset($_SESSION['user_id']);
+
+}
+
+header("Location: login.php");
+die;
